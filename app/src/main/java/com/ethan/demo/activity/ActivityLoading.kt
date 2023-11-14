5d9245a71c52e20f1abedde9e2b16459233c69c0
Ethan <ethann@petalmail.com>
2023-11-14 14:17:50 +0800

    :sparkles: 添加用力。

--- app/src/main/java/com/ethan/demo/activity/ActivityLoading.kt
+++ app/src/main/java/com/ethan/demo/activity/ActivityLoading.kt
@@ -5,15 +5,29 @@ import android.content.Intent
 import android.os.Bundle
 import com.ethan.demo.R
 import com.ethan.demo.base.ActivityBase
+import com.ethan.demo.databinding.ActivityLoadingBinding
+import com.ethan.zincum.dialog.IOSLoadingDialog
 
 class ActivityLoading : ActivityBase() {
+    private lateinit var mBinding: ActivityLoadingBinding
+
     companion object {
         fun jump(context: Context) {
             context.startActivity(Intent(context, ActivityLoading::class.java))
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_huawei_loading)
+        mBinding = ActivityLoadingBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        mBinding.includeToolbar.toolbar.title = "Loading"
+        setSupportActionBar(mBinding.includeToolbar.toolbar)
+        mBinding.includeToolbar.toolbar.setNavigationIcon(R.drawable.nav_back)
+        mBinding.includeToolbar.toolbar.setNavigationOnClickListener { finish() }
+
+        mBinding.btnDialogIos.setOnClickListener {
+            val dialog = IOSLoadingDialog().setOnTouchOutside(true).showNow(supportFragmentManager, "IosLoadingDialog")
+        }
     }
 }
---
+++ library/src/main/java/com/ethan/zincum/dialog/IOSLoadingDialog.kt
@@ -0,0 +1,75 @@
+package com.ethan.zincum.dialog
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import com.ethan.zincum.R
+
+class IOSLoadingDialog : DialogFragment(), DialogInterface.OnKeyListener {
+    /**
+     * 默认点击外面无效
+     */
+    private var onTouchOutside = false
+
+    /**
+     * 加载框提示信息 设置默认
+     */
+    private var hintMsg = "加载中"
+
+    /**
+     * 设置是否允许点击外面取消
+     *
+     * @param onTouchOutside x
+     * @return
+     */
+    fun setOnTouchOutside(onTouchOutside: Boolean): IOSLoadingDialog {
+        this.onTouchOutside = onTouchOutside
+        return this
+    }
+
+    /**
+     * 设置加载框提示信息
+     *
+     * @param hintMsg
+     */
+    fun setHintMsg(hintMsg: String): IOSLoadingDialog {
+        if (!hintMsg.isEmpty()) {
+            this.hintMsg = hintMsg
+        }
+        return this
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val dialog = dialog
+
+
+        dialog.let {
+            it?.window?.let { window ->
+                window.setBackgroundDrawableResource(android.R.color.transparent)
+                it.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                it.setCanceledOnTouchOutside(onTouchOutside) //不响应返回键
+                it.setOnKeyListener(this)
+            }
+        }
+
+
+        // 设置背景透明
+        // 去掉标题
+        val loadingView = inflater.inflate(R.layout.dialog_ios_loading, container)
+        val hintTextView = loadingView.findViewById<TextView>(R.id.tv_ios_loading_dialog_hint)
+        hintTextView.text = hintMsg
+
+
+        return loadingView
+    }
+
+    override fun onKey(dialog: DialogInterface, keyCode: Int, event: KeyEvent): Boolean {
+        return keyCode == KeyEvent.KEYCODE_BACK
+    }
+}
